bfc9f6985a699c4fcce97208e312ff706554fdc7
zhangt <atehzhang000@163.com>
2019-02-14 17:15:14 +0800

    commit test

---
+++ MyBootTest/src/main/java/com/myboot/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.myboot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyBootTestApplication.class);
+	}
+
+}
+
